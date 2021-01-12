7483f992f5a92c38287750cde849ebf16fb4f82a
bTsuyoi <gubbala.saikishore@gmail.com>
2021-01-12 10:23:42 +0530

    REFACTORED:  code to write a Class Method to Compute EmpWage.

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -5,19 +5,26 @@ public class EmpWageBuilder {
 	public static final int EMP_RATE_PER_HOUR = 20;
 	public static final int NUM_OF_WORKING_DAYS = 5;
 	public static final int MAX_HRS_IN_MONTH = 10;
+	public static int empHrs = 0;
+	public static int empWage = 0;
+
+	public static int findEmpWage() {
+		empWage =  empHrs * EMP_RATE_PER_HOUR;
+        return empWage;
+    }
+
 	public static void main(String[] args) {
 
-		int empHrs =0;
-		int empWage = 0;
 		int totalEmpWage =0;
 		int totalEmpHrs = 0;
 		int totalWorkingDays = 0;
 
-		while (totalEmpHrs <= MAX_HRS_IN_MONTH &&	totalWorkingDays < NUM_OF_WORKING_DAYS) {
+		while (totalEmpHrs <= MAX_HRS_IN_MONTH &&
+				totalWorkingDays < NUM_OF_WORKING_DAYS) {
 
 			totalWorkingDays++;
 
-	 		double empCheck = Math.floor(Math.random() * 10) % 3;
+			double empCheck = Math.floor(Math.random() * 10) % 3;
 
 			switch ((int) empCheck) {
 				case IS_FULL_TIME:
@@ -30,11 +37,9 @@ public class EmpWageBuilder {
 					empHrs = 0;
 			}
 			totalEmpHrs += empHrs;
-			empWage = empHrs * EMP_RATE_PER_HOUR;
-			totalEmpWage += empWage;
-			System.out.println("Emp Wage: " + empWage);
+			totalEmpWage += findEmpWage();
+			System.out.println("Emp Wage: " + findEmpWage());
 		}
 		System.out.println("Total Emp Wage: " + totalEmpWage);
 	}
 }
-
